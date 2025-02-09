8382b9913b28ba94c8e2377feb18983c5b20508b
ryanmiura <ryanmiuracarrasco@hotmail.com>
2025-02-08 21:23:49 -0300

    feat: implementando admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,13 +1,32 @@
 <template>
-    <v-container fluid class="pa-0">
-        <span>TESTEEEEEEEE</span>
-      <div class="bg-background pa-16">
-      </div>
-      <BairrosGrid />
-    </v-container>
-  </template>
+  <v-container fluid class="pa-0">
+    <v-tabs v-model="tab">
+      <v-tab>Bairros</v-tab>
+      <v-tab>Pontos de Coleta</v-tab>
+      <v-tab>Posts</v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <bairros-management v-if="tab === 0" />
+      </v-tab-item>
+      <v-tab-item>
+        <pontos-coleta-management v-if="tab === 1" />
+      </v-tab-item>
+      <v-tab-item>
+        <posts-management v-if="tab === 2" />
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
+</template>
 
 <script setup>
+import { ref } from 'vue';
+import BairrosManagement from '@/components/BairrosManagement.vue';
+import PontosColetaManagement from '@/components/PontosColetaManagement.vue';
+import PostsManagement from '@/components/PostsManagement.vue';
+
+const tab = ref(0);
 </script>
 
 <style scoped>
