6e8732c4e0c758df1f3ffcc083ace6dfd8d5f452
pedrohcmoda <phcunha47@gmail.com>
2025-02-12 09:39:28 -0300

    fix - alterando formatacao e protecao de rota

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 import HomePrincipal from '../views/HomePrincipal.vue'
 import Blog from '../views/Blog.vue'
@@ -9,40 +10,24 @@ import Admin from '@/views/Admin.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomePrincipal
-        },
-        {
-            path: '/blog',
-            name: 'blog',
-            component: Blog
-        },
-        {
-            path: '/coleta-seletiva',
-            name: 'coleta-seletiva',
-            component: ColetaSeletiva
-        },
-        {
-            path: '/pontos-coleta',
-            name: 'pontos-coleta',
-            component: PontosColeta
-        },
-        {
-            path: '/admin',
-            name: 'admin',
-            component: Admin,
-            meta: { requiresAuth: true }
-        },
+        { path: '/', name: 'home', component: HomePrincipal },
+        { path: '/blog', name: 'blog', component: Blog },
+        { path: '/coleta-seletiva', name: 'coleta-seletiva', component: ColetaSeletiva },
+        { path: '/pontos-coleta', name: 'pontos-coleta', component: PontosColeta },
+        { path: '/admin', name: 'admin', component: Admin, /*meta: { requiresAuth: true } */ },
     ]
 })
 
 router.beforeEach((to, from, next) => {
-    const isAuthenticated = firebase.auth().currentUser !== null
-
-    if (to.meta.requiresAuth && !isAuthenticated) {
-        next('/')
+    if (to.meta.requiresAuth) {
+        const auth = getAuth()
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                next()
+            } else {
+                next('/')
+            }
+        })
     } else {
         next()
     }
