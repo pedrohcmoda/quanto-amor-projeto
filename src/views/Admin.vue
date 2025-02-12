2757fd4a6aca52d8201d27d3b91c6dd67f62c1b2
pedrohcmoda <phcunha47@gmail.com>
2025-02-12 06:39:11 -0300

    feature - tabela de auditoria

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -5,6 +5,7 @@
         <v-tab>Bairros</v-tab>
         <v-tab>Pontos de Coleta</v-tab>
         <v-tab>Posts</v-tab>
+        <v-tab>Auditoria</v-tab>
       </v-tabs>
     </div>
   
@@ -19,6 +20,9 @@
         <v-tab-item>
           <posts-management v-if="tab === 2" />
         </v-tab-item>
+        <v-tab-item>
+          <auditoria-management v-if="tab === 3" />
+        </v-tab-item>
       </v-tabs-items>
     </div>
   </v-container>
@@ -29,6 +33,7 @@ import { ref } from 'vue';
 import BairrosManagement from '@/components/BairrosManagement.vue';
 import PontosColetaManagement from '@/components/PontosColetaManagement.vue';
 import PostsManagement from '@/components/PostsManagement.vue';
+import AuditoriaManagement from '@/components/AuditoriaManagement.vue';
 
 const tab = ref(0);
 </script>
